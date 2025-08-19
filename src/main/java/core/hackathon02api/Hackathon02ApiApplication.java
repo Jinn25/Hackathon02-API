d4d2db599e9dfb3244b912004ab32fc608051948
Jinn25 <hyojinn82@gmail.com>
2025-08-19 21:27:20 +0900

    Initial commit

---
+++ src/main/java/core/hackathon02api/Hackathon02ApiApplication.java
@@ -0,0 +1,13 @@
+package core.hackathon02api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackathon02ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hackathon02ApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/core/hackathon02api/Hackathon02ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package core.hackathon02api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackathon02ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
