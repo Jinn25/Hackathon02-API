6dc1d358bfd4907c260d28ff0ef52f3fde660df6
suho-lee05 <suhoi6791@gmail.com>
2025-08-27 19:07:35 +0900

    security_config에 CORS문제 해결

--- src/main/java/core/hackathon02api/auth/security/config/SecurityConfig.java
+++ src/main/java/core/hackathon02api/auth/security/config/SecurityConfig.java
@@ -12,6 +12,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -22,6 +25,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(List.of("http://localhost:3000",
+                            "https://dododokk.github.io")); // 프론트 주소
+                    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                    config.setAllowedHeaders(List.of("*"));
+                    config.setAllowCredentials(true); // Authorization 허용
+                    config.setExposedHeaders(List.of("Authorization"));
+                    return config;
+                }))
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
@@ -31,7 +44,7 @@ public class SecurityConfig {
                         .requestMatchers(org.springframework.http.HttpMethod.GET, "/api/posts/**").permitAll() // ✅ 공개 조회
                         .requestMatchers(org.springframework.http.HttpMethod.GET, "/api/posts/**").permitAll()
                         .requestMatchers("/api/posts/*/applications/**").permitAll()
-
+                        .requestMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/ws/**").permitAll() // ✅ 웹소켓 허용
                         .requestMatchers("/api/notifications/**").authenticated() // 알림은 인증 필요
                         .anyRequest().permitAll()
